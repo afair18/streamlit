3ecf890518ff6079e0ebf52e6bfdf5c7d3e1dc09
user afair18 <user afair18@naver.com>
2024-01-31 01:10:22 +0900

    add main-fastapi

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class User(BaseModel):
-    name: str
-    age: int
+    ticker: str
+    price: float
 
 @app.post("/submit-user")
-async def submit_user(user: User):
-    return {"message": f"이름: {user.name}, 나이: {user.age}"}
+def submit_user(user: User):
+    print(f"Received user data: 종목 - {user.ticker}, 가격 - {user.price}")
+    return {"message": f"종목: {user.ticker}, 가격: {user.price}"}
