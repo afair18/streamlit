08f805fd386d90f505276ec5d40aec3a75530c04
user afair18 <user afair18@naver.com>
2024-01-31 22:05:59 +0900

    del

--- bitgetpost.py
+++ bitgetpost.py
@@ -7,7 +7,7 @@ class Data(BaseModel):
     ticker: str
     price: float
 
-@app.post("/")
+@app.post("/data")
 def submit_user(user: Data):
     print(f"Received user data: 종목 - {user.ticker}, 가격 - {user.price}")
     return {"message": f"종목: {user.ticker}, 가격: {user.price}"}
